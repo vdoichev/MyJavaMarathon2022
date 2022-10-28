7a3855e08a8d7d3c90657116d4f21cc38272adcc
v.doichev <siavaismail@gmail.com>
2022-10-28 14:15:18 +0300

    Task1 for Day 17

--- src/Main/day17/ChessPiece.java
+++ src/Main/day17/ChessPiece.java
@@ -1,19 +1,30 @@
 package Main.day17;
+/*
+1. Создайте Enum “Шахматная фигура” (англ. ChessPiece).
+Всего существует 6 различных шахматных фигур: король (англ. king), ферзь (англ. queen), ладья (англ. rook),
+ слон (англ. bishop), конь (англ. knight) и пешка (англ. pawn). В нашей программе, у каждой шахматной
+ фигуры есть следующие характеристики:
+● Ценность фигуры - вещественное число
+● Строковое обозначение фигуры
+В нашем перечислении необходимо создать 12 фигур - 6 белых и 6 черных. Названия фигур в перечислении
+должны иметь следующий вид:
+KING_WHITE, KING_BLACK, QUEEN_WHITE, QUEEN_BLACK, и так далее
+У каждой фигуры должны быть заданы две характеристики - ее ценность и строковое обозначение.
+ */
 
 public enum ChessPiece {
-    KING_BLACK(100.0,"\u2654"),
-    QUEEN_BLACK(9,"\u2655"),
-    ROOK_BLACK(5,"\u2656"),
-    BISHOP_BLACK(3.5,"\u2657"),
-    KNIGHT_BLACK(3,"\u2658"),
-    PAWN_BLACK(1,"\u2659"),
-
-    KING_WHITE(100.0,"\u265A"),
-    QUEEN_WHITE(9,"\u265B"),
-    ROOK_WHITE(5,"\u265C"),
-    BISHOP_WHITE(3.5,"\u265D"),
-    KNIGHT_WHITE(3,"\u265E"),
-    PAWN_WHITE(1,"\u265F")
+    KING_WHITE(100.0,"\u2654"),
+    QUEEN_WHITE(9,"\u2655"),
+    ROOK_WHITE(5,"\u2656"),
+    BISHOP_WHITE(3.5,"\u2657"),
+    KNIGHT_WHITE(3,"\u2658"),
+    PAWN_WHITE(1,"\u2659"),
+    KING_BLACK(100.0,"\u265A"),
+    QUEEN_BLACK(9,"\u265B"),
+    ROOK_BLACK(5,"\u265C"),
+    BISHOP_BLACK(3.5,"\u265D"),
+    KNIGHT_BLACK(3,"\u265E"),
+    PAWN_BLACK(1,"\u265F")
     ;
 
     private double value;
--- src/Main/day17/Task1.java
+++ src/Main/day17/Task1.java
@@ -1,10 +1,14 @@
 package Main.day17;
+/*
+Используя созданный enum, в методе main() класса Task1 создайте массив, содержащий 4 белых пешки и 4 черных
+ладьи. Выведите содержимое этого массива на экран, используя строковые обозначения шахматных фигур.
+ */
 
 public class Task1 {
     public static void main(String[] args) {
         ChessPiece[] pieces = new ChessPiece[8];
         for (int i = 0; i < pieces.length; i++) {
-            pieces[i] = (i < 4) ? ChessPiece.PAWN_BLACK : ChessPiece.ROOK_WHITE;
+            pieces[i] = (i < 4) ? ChessPiece.PAWN_WHITE : ChessPiece.ROOK_BLACK;
         }
         for (ChessPiece piece : pieces) {
             System.out.print(piece.getDescription() + " ");
